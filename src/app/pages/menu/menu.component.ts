d6a6f8d19506e1653b6acac24615f5fcae727906
Abdelaziz mohamed <abdelazizkishk19@gmail.com>
2025-06-01 17:18:05 +0300

    commit

--- src/app/pages/menu/menu.component.ts
+++ src/app/pages/menu/menu.component.ts
@@ -1,4 +1,5 @@
-import { AfterViewInit, Component } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
+import { AfterViewInit, Component, inject, PLATFORM_ID } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import * as AOS from 'aos';
 
@@ -9,15 +10,18 @@ import * as AOS from 'aos';
   styleUrl: './menu.component.css',
 })
 export class MenuComponent implements AfterViewInit {
+  private readonly pLATFORM_ID = inject(PLATFORM_ID);
   ngAfterViewInit(): void {
-    AOS.init({
-      offset: 100, // offset (in px) from the original trigger point
-      duration: 800, // animation duration
-      easing: 'ease-in-out',
-      delay: 100,
-      once: false, // animate on every scroll
-      mirror: true, // animate out when scrolling past the element
-    });
-    AOS.refresh();
+    if (isPlatformBrowser(this.pLATFORM_ID)) {
+      AOS.init({
+        offset: 100, // offset (in px) from the original trigger point
+        duration: 800, // animation duration
+        easing: 'ease-in-out',
+        delay: 100,
+        once: false, // animate on every scroll
+        mirror: true, // animate out when scrolling past the element
+      });
+      AOS.refresh();
+    }
   }
 }
